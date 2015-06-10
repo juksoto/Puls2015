21926b9ab0233d07e678b7f3958bb8ae4122ad71
juksoto <juksoto@gmail.com>
2015-06-10 09:52:14 -0500

    Cmabios
    cmaiu

---
+++ Github.php
@@ -0,0 +1,50 @@
+DISEÑO WEB
+
+Frontend -> Interaccion directa con el usuario
+HTML no es un lenguaje de programación.
+HTML _> Lenguaje de modelaje de información
+CSS -> Aplicar diseño gráfico a html
+JS-> Lenguaje de programación.  Derivado de la sintaxis de C++
+Backend ->  
+	PHP, .NET, ASP3 , Ruby Rails, Python, Node.js del lado al servidor.
+Base de Datos ->
+	MySQL -> Relacional
+Postgres
+Django -> No relacional
+
+FTP -> Sistema de transmisión de archivos
+Hosting -> public_html / - FTP
+	Index.php
+	Images
+	Js
+www.dogecoin.com
+Problemas
+1.	Múltiples personas en el equipo
+2.	NO HAY CULPA
+3.	Arcano
+Sistema de control de versiones
+SVN -> Subversion / Mercurial / TFS / CVS / GIT
+Github -> Aplicación web -> 
+1.	Red social de programadores
+2.	Web / APPS / 
+3.	Mucho más fácil usar git
+--
+Hi 
+Repositorio de códigos  Github (Master)
+Clon local
+	Commit  –  sync
+Ventjas
+-	Múltiples versiones de código
+-	Base de datos atómica - > Cambios delta
+o	Base de datos atómica del código
+-	Historial de cada archivo
+-	Múltiples personas trabajan sobre el mismo archivo
+-	CULPA al CULPABLE
+-	Ramas - > branches
+o	* Master (GITHUB.COM) REPO PRINCIPAL
+o	Master (DD) REPO LOCAL
+	Master
+•	MERGE (experimental - merge)
+	Experimental
+o	Master (servidor.com) REPO PRODUCCION
+
